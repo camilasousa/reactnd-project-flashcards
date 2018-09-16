03936ef3e605e2a1c03e224c417ef02d737edb06
camilasousa <miyuki.sousa@gmail.com>
2018-09-16 17:29:52 -0300

    feature: show deck info on DeckView

--- components/DeckView/DeckView.js
+++ components/DeckView/DeckView.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, Text, TouchableOpacity } from 'react-native';
 
-const DeckView = () => (
-  <View>
-    <Text>
-      DeckView
-    </Text>
-  </View>
-);
+import styles from './styles';
+
+const DeckView = ({ navigation }) => {
+  const { deck } = navigation.state.params;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        {deck.title}
+      </Text>
+      <Text style={styles.number}>
+        {deck.questions ? deck.questions.length : 0} decks
+      </Text>
+
+      <TouchableOpacity style={styles.addCardButton}>
+        <Text style={styles.addCardButtonText}>
+          Add Card
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+DeckView.propTypes = {
+  navigation: PropTypes.shape({
+    state: PropTypes.shape({
+      params: PropTypes.shape({
+        deck: PropTypes.shape({
+          title: PropTypes.string.isRequired,
+          questions: PropTypes.arrayOf(PropTypes.shape({})),
+        }).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default DeckView;
---
+++ components/DeckView/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: 'center',
+  },
+  title: {
+    marginTop: 30,
+    fontSize: 34,
+  },
+  number: {
+    fontSize: 16,
+  },
+  addCardButton: {
+    marginTop: 50,
+    paddingTop: 6,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 6,
+    backgroundColor: 'blue',
+  },
+  addCardButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
+
+export default styles;
