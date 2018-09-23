bba2caeeff185c588b5483de3dce7a3a0484d0e6
camilasousa <miyuki.sousa@gmail.com>
2018-09-22 21:23:18 -0300

    fix: not updating deck after inserting question

--- components/DeckView/DeckView.js
+++ components/DeckView/DeckView.js
@@ -1,45 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
 
 import styles from './styles';
 
-const DeckView = ({ navigation }) => {
-  const { deck } = navigation.state.params;
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>
-        {deck.title}
-      </Text>
-      <Text style={styles.number}>
-        {deck.questions ? deck.questions.length : 0} decks
-      </Text>
+const DeckView = ({ navigation, deck }) => (
+  <View style={styles.container}>
+    <Text style={styles.title}>
+      {deck.title}
+    </Text>
+    <Text style={styles.number}>
+      {deck.questions ? deck.questions.length : 0} decks
+    </Text>
 
-      <TouchableOpacity
-        style={styles.addCardButton}
-        onPress={() => navigation.navigate('AddQuestion', { deckKey: deck.title })}
-      >
-        <Text style={styles.buttonText}>
-          Add Card
-        </Text>
-      </TouchableOpacity>
+    <TouchableOpacity
+      style={styles.addCardButton}
+      onPress={() => navigation.navigate('AddQuestion', { deckKey: deck.title })}
+    >
+      <Text style={styles.buttonText}>
+        Add Card
+      </Text>
+    </TouchableOpacity>
 
-      {
-        deck.questions && deck.questions.length > 0
-          ? (
-            <TouchableOpacity
-              style={styles.startQuizButton}
-              onPress={() => navigation.navigate('Quiz', { questions: deck.questions })}
-            >
-              <Text style={styles.buttonText}>
-                Start Quiz
-              </Text>
-            </TouchableOpacity>
-          )
-          : null
-      }
-    </View>
-  );
+    {
+      deck.questions && deck.questions.length > 0
+        ? (
+          <TouchableOpacity
+            style={styles.startQuizButton}
+            onPress={() => navigation.navigate('Quiz', { questions: deck.questions })}
+          >
+            <Text style={styles.buttonText}>
+              Start Quiz
+            </Text>
+          </TouchableOpacity>
+        )
+        : null
+    }
+  </View>
+);
+
+
+const mapStateToProps = (state, ownProps) => {
+  const { deck } = ownProps.navigation.state.params;
+  return {
+    deck: state.decksById[deck.title],
+  };
 };
 
 DeckView.propTypes = {
@@ -48,11 +54,14 @@ DeckView.propTypes = {
       params: PropTypes.shape({
         deck: PropTypes.shape({
           title: PropTypes.string.isRequired,
-          questions: PropTypes.arrayOf(PropTypes.shape({})),
         }).isRequired,
       }).isRequired,
     }).isRequired,
   }).isRequired,
+  deck: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    questions: PropTypes.arrayOf(PropTypes.shape({})),
+  }).isRequired,
 };
 
-export default DeckView;
+export default connect(mapStateToProps)(DeckView);
